306366328b1420f157f81e77dfb1b4e2895817da
Umakshi12 <66000862+Umakshi12@users.noreply.github.com>
2021-10-22 22:54:13 +0530

    Hexagon_Turtle.py
    This is the python code for generating a hexagon

---
+++ Hexagon_Turtle.py
@@ -0,0 +1,16 @@
+# Draw a hexagon
+from turtle import *
+
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+left(60)
+forward(150)
+
+bye()
